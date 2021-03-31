70342fad6c82c56b446e286f6a6dd238549cd63b
CHADAD <mohamed.chadad@nimbleways.com>
2021-04-01 00:56:50 +0100

    add router

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import './App.css';
 import {BankSelector} from "./pages/BankSelector/BankSelector";
 import {AppContainer} from "./App.style";
 import PayComp from "./components/Pay/PayComp";
+import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
+import {Home} from "./pages/home/home";
+
 
 function App() {
   return (
     <AppContainer>
-        <PayComp />
+        <Router>
+            <Switch>
+                <Route path="/pay">
+                    <PayComp />
+                </Route>
+                <Route path="/banks">
+                    <BankSelector />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
     </AppContainer>
 
   );
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -4,6 +4,9 @@ export const Home = () => {
     return (
         <div>
             Home
+            <br/>
+            <a href="/banks">banks</a>
+            <a href="/pay">pay</a>
         </div>
     )
 }
